ca4bddada1f40de1cd617ed2101781cae2ccd0b9
Ian Norris <ian.norris.1991@gmail.com>
2015-07-17 19:38:22 -0700

    refactored into using directives, added styling, added all data

--- app/assets/javascripts/angular/chopblendmash.js
+++ app/assets/javascripts/angular/chopblendmash.js
@@ -1,34 +1,77 @@
-var myApp = angular.module('chopblendmash', ['ngRoute', 'ngResource', 'ngAnimate']); 
-myApp.controller('RecipeController', ['$scope', function($scope) {
-  $scope.preparationTechniques = ["Chop","Tear","Toss","Blend","Boil","Mash","Slice","Layer","Roast","Drizzle","Massage"];
-  $scope.stylingTechniques = [{name: "Sour", details: "This is the way to prepare sour foods."},{name: "Salty", details: "This is the way to prepare salty foods."}];
-  $scope.currentStylingIndex = 0;
-  $scope.currentPreparationIndex = 0;
-  $scope.currentStylingTechnique = $scope.stylingTechniques[0];
-  $scope.currentPreparationTechnique = $scope.preparationTechniques[0];
-  $scope.switchStylingTechnique = function() {
-    $scope.currentStylingIndex = ($scope.currentStylingIndex + 1) % $scope.stylingTechniques.length;
-  }
-    $scope.x = {edit: false};
-  $scope.switchPreparationTechnique = function() {
-     $scope.currentPreparationIndex = ($scope.currentPreparationIndex + 1) % $scope.preparationTechniques.length;
-  }
-  $scope.displayCurrentStyling = function(index) {
-    return index === $scope.currentStylingIndex;
-  }
-  $scope.displayCurrentTechnique = function(index) {
-    return index === $scope.currentPreparationIndex;
-  }
+var exampleApp = angular.module("chopblendmash", ["ngAnimate", "ngRoute"]);
+exampleApp.directive('scroller',function() {
+  return {
+    transclude: true,
+    templateUrl: '/templates/scroller.html',
+    scope: {
+      techniques: '='
+    },
+    link: function(scope, elem, attrs, ctrl, transcludeFn) {
+      scope.currentIndex = 0;
+      scope.test = 1;
+      scope.currentTechnique = scope.techniques && scope.techniques[0];
+      scope.switchTechnique = function() {
+        scope.currentIndex = (scope.currentIndex + 1) % scope.techniques.length;
+        scope.currentTechnique = scope.techniques[scope.currentIndex];
+      }
+      scope.displayCurrentTechnique = function(index) {
+        return index === scope.currentIndex;
+      }
+    }
+  };
+});
+exampleApp.controller('RecipesController', ['$scope', function($scope) {
+  $scope.preparationTechniques = [{name: "Chop", description: "Create a one of a kind salad! Just chop all ingredients, toss with dressing and enjoy."},
+  {name:"Blend", description: "Make a savory soup by boiling the ingredients and putting them plus 2 cups water and the sauce in the blender." },
+  {name: "Mash", description: "Like mashed potatoes only better! Boil all ingredients, drain the water, add the sauce and mash them for a tasty glop."},
+  {name: "Layer", description: "Make your own sandwich masterpiece. Thinly slice the first 2 ingredients and layer with the third between bread with a little sauce."},
+  {name:"Roast",description:"For an easy dinner, simply mix or layer all ingredients in an oven-safe, oiled baking dish and bake at 350 degrees until tender."},
+  {name: "Massage", description: "Instead of back massage try a salad assage! Chop all ingredients, drizzle with a (M) sauce and massage it with your hands for about a minute."},
+  {name: "Cook", description: "Yum! Just chop all ingredients and add to an oiled, medium-high heated pan. Cook til slightly brown. Turn off heat, add sauce, and mix."}];
+  $scope.stylingTechniques = [
+  {name: "Sour", ingredients: ["1 part honey", "2 parts frozen orange juice", "10 parts plain yogurt", "cinnamon to taste"]},
+  {name: "Sour", ingredients: ["1 part chopped dill", "2 parts lemon or dill pickle juice", "3 parts mayonaise"]},
+  {name: "Salty", ingredients: ["1 part honey", "2 parts frozen orange juice", "10 parts plain yogurt", "cinnamon to taste"]},
+  {name: "Savory", ingredients: ["1 part honey", "2 parts frozen orange juice", "10 parts plain yogurt", "cinnamon to taste"]},
+  {name: "Tart", ingredients: ["1 part yogurt", "1 part shredded cucumber", "salt to taste", "lemon juice to taste", "chopped fresh dill to taste"]}
+  ];
+  $scope.bodyIngredients = [
+    "Ground Beef",
+    "Cheese",
+    "Chicken",
+    "Eggs",
+    "Beans",
+    "Bacon",
+    "Nuts"
+  ];
+  $scope.sideIngredients = [
+  "Carrot",
+  "Broccoli",
+  "Apple",
+  "Berries",
+  "Zucchini",
+  "Tomato",
+  "Beets"
+  ];
+  $scope.vegetableBase = [
+    "Kale",
+    "Spinach",
+    "Basil",
+    "Cabbage",
+    "Arugula",
+    "Leaf Lettuce",
+    "Rainbow Chard"
+  ];
 }]);
-myApp.config([
+exampleApp.config([
     '$routeProvider', function($routeProvider) {
     	$routeProvider.when('/', {
     		templateUrl: '/templates/recipes.html',
-    		controller: 'RecipeController'
+    		controller: 'RecipesController'
     	});
         $routeProvider.otherwise({
             templateUrl: '/templates/recipes.html',
-            controller: 'RecipeController'
+            controller: 'RecipesController'
         });
     }
 ]);
