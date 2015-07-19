1b267d8aa4c6311abb9f57b5d407e6b04d498de0
Ian Norris <ian.norris.1991@gmail.com>
2015-07-18 22:12:32 -0700

    adding ability to create a shareable link

--- app/assets/javascripts/angular/chopblendmash.js
+++ app/assets/javascripts/angular/chopblendmash.js
@@ -8,13 +8,11 @@ exampleApp.directive('scroller',function() {
       currentIndex: '='
     },
     link: function(scope, elem, attrs, ctrl, transcludeFn) {
-      scope.currentIndex = 0;
-      scope.test = 1;
       scope.currentTechnique = scope.techniques && scope.techniques[0];
       scope.switchTechnique = function() {
         scope.currentIndex = (scope.currentIndex + 1) % scope.techniques.length;
       }
-      scope.$watch('currentIndex', function(newValue) {
+      scope.$watch('currentIndex', function(newValue, oldValue) {
         scope.currentTechnique = scope.techniques[newValue];
       });
       scope.displayCurrentTechnique = function(index) {
@@ -23,7 +21,22 @@ exampleApp.directive('scroller',function() {
     }
   };
 });
-exampleApp.controller('RecipesController', ['$scope', function($scope) {
+exampleApp.controller('RecipesController', ['$scope','$routeParams', function($scope, $routeParams) {
+    $scope.indexForPreparation = parseInt($routeParams.prepId) || 0;
+    $scope.indexForStyling = parseInt($routeParams.stylingId) || 0;
+    $scope.indexForBody = parseInt($routeParams.bodyId) || 0;
+    $scope.indexForSide = parseInt($routeParams.sideId) || 0;
+    $scope.indexForVegetableBase = parseInt($routeParams.vegetableId) || 0;
+  
+  $scope.saveLink = function() {
+    alert(buildLink($scope.indexForPreparation, $scope.indexForStyling, $scope.indexForBody, $scope.indexForSide, $scope.indexForVegetableBase));
+  }
+
+  function buildLink(prepId, stylingId, bodyId, sideId, vegetableId) {
+    var linkWithoutParams = window.location.href.split("?")[0];
+    return linkWithoutParams + "?prepId=" + prepId + "&stylingId=" + stylingId + "&bodyId=" + bodyId + "&sideId=" + sideId + "&vegetableId=" + vegetableId;
+  }
+
   $scope.makeRandom = function() {
     $scope.indexForPreparation = random($scope.preparationTechniques.length);
     $scope.indexForStyling = random($scope.stylingTechniques.length);
